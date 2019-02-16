4e60261abf4a971ecd383afd6f24c74e58bbf715
Chris LeBaron <chrislebaron@gmail.com>
2019-02-16 14:55:27 -0700

    Create blog and list blog working again

--- README.md
+++ README.md
@@ -31,9 +31,10 @@ This uploads your template to an S3 bucket and deploys the specified resources u
 
 ## Setting up DynamoDB Locally ##
 
-You can run DynamoDB locally too, but you'll need to download and install a .jar package to get it running. The AWS documentation is here: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html#DynamoDBLocal.DownloadingAndRunning
+You can run DynamoDB locally too:
+https://hub.docker.com/r/amazon/dynamodb-local/
 
-Make sure that in any serverless functions that use DyanmoDB that you update the local config to point to the correct port. By default DynamoDB will run on port 8000, but I have it configured to run on port 8081 on my machine.
+Make sure that in any serverless functions that use DyanmoDB that you update the local config to point to the correct port. By default DynamoDB will run on port 8000.
 
 Another note with trying to access the local DynamoDB, that because it's running in docker, you'll notice that we don't access it from the Lambda function with `http://localhost:8000`, you need to to access `http://docker.for.mac.localhost:8081` or it will not work.
 
--- blog/create.js
+++ blog/create.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const slugify = require('slugify');
 let table = "";
 
 if(process.env.AWS_SAM_LOCAL) {
@@ -16,11 +17,19 @@ if(process.env.AWS_SAM_LOCAL) {
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
     console.log("Event", event);
 
     let body = JSON.parse(event.body);
     body.createdAt = Math.floor(Date.now() / 1000);
+
+    if(!body.title && !body.content) {
+        return callback({statusCode: 400, body: {message: 'Invalid Request! "title" and "content" fields required'}})
+    }
+
+    body.slug = await slugify(body.title, {lower: true});
+
+
     const params = {
         TableName: table,
         Item: body
@@ -28,23 +37,12 @@ exports.handler = (event, context, callback) => {
 
     console.log("Params: ", params);
 
-    dynamodb.put(params, function(err, data){
-        if(err){
-            console.error('Unable to add item. ', err);
-            callback(err, null);
-        }else{
-            console.log("Data from Dynamo", data);
-            callback(null,
-                {
-                    statusCode: 200,
-
-                    body: JSON.stringify({message: "Hi from create", data})
-                }
-            );
-        }
-    });
-
-
-
-
+    try {
+        const createdArticle = await dynamodb.put(params).promise();
+        console.log('We created this article!', createdArticle)
+        return callback(null, {statusCode: 200, body: createdArticle});
+    }catch (e) {
+        console.error(e);
+        return callback(e);
+    }
 }
